d52f7724456af542b5ad9723e34979b443d86cb2
Vedant Shah <87229097+vedant-shah@users.noreply.github.com>
2023-05-02 23:32:08 +0530

    Update and rename Bitstuffing.c to p1b.c

---
+++ p1b.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int n, i;
+    printf("Enter size of frame");
+    scanf("%d",&n);
+    int frame[n];
+    printf("Enter the frame\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&frame[i]);
+    }
+    int count=0;
+    for(i=0;i<n;i++)
+    {
+        if(count==5&&frame[i]==1)
+        {
+            printf("01");
+            count=1;
+        }
+        else if(count<5&&frame[i]==1)
+        {
+            count++;
+            printf("1");
+        }
+        else if(frame[i]==0)
+        {
+            printf("0");
+            count=0;
+        }
+    }
+}
